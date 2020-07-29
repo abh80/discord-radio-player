df1810740155d256ea3877f91a3564e28ff59a6c
Sfue <50198413+abh80@users.noreply.github.com>
2020-07-29 10:32:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -4,10 +4,15 @@ A radio player module for discord which has custom ffmpeg args support.
 ```bash
 npm i discord-radio-player
 ```
+# Installing Required Dependencies
+```bash
+npm i @discordjs/opus ffmpeg-static
+```
+
 # Searching Radio
 ```js
 const radio = require('discord-radio-player')
-const search = await radio.Radio.search({serchterm:'kissfm',limit:1})
+const search = await radio.Radio.search({serchterm:'kissfm',limit:1,by:'name'}) //returns radio results by name
 ```
 # Playing Radio from search results
 ```js
@@ -32,6 +37,10 @@ let stream = radio.Radio.getStream(streamUrl,{filters:FFmpegfilters}) //defining
 message.member.voice.channel.join().then(c=>c.play(stream,{type:'opus'}))
 //starts playing radio
 ```
+# Setting Volume
+```js
+let stream = radio.Radio.getStream(streamUrl,{volume:2}) // 1 is default volume and 2 is double volume whereas 0.5 is half volume
+```
 # Pre Existing Filters
 - Bassboost
 ```js
@@ -50,4 +59,16 @@ message.member.voice.channel.join().then(c=>c.play(stream,{type:'opus'}))
 let streamUrl = search[0].url_resolved
 // Adding 8D effect
 let stream = radio.Radio.getStream(streamUrl,{'8d':true}) //generates 8d effect x)
+```
+- Karaoke
+```js
+let streamUrl = search[0].url_resolved
+// Adding karaoke effect
+let stream = radio.Radio.getStream(streamUrl,{karaoke:true}) //generates karaoke like effect
+```
+- Pulsator
+```js
+let streamUrl = search[0].url_resolved
+// Adding pulsator effect
+let stream = radio.Radio.getStream(streamUrl,{pulsator:true}) //generates pulsator effect
 ```
--- src/Radio.ts
+++ src/Radio.ts
@@ -34,15 +34,24 @@ export default class {
     if (options && options.filters && Array.isArray(options.filters)) {
       FFmpeg = FFmpeg.concat(options.filters);
     }
-    if(options && options.filters && options.bassboost&&!isNaN(options.bassboost)){
-      let bass = options.bassboost
-      let bassSetting = ["-af",`bass=g=${bass}`]
-      if(options.normalizer)bassSetting = ["-af",`bass=g=${bass},dynaudnorm=f=200`]
-      FFmpeg = FFmpeg.concat(bassSetting)
+    if (options && options.bassboost && !isNaN(options.bassboost)) {
+      let bass = options.bassboost;
+      let bassSetting = ["-af", `bass=g=${bass}`];
+      if (options.normalizer)
+        bassSetting = ["-af", `bass=g=${bass},dynaudnorm=f=200`];
+      FFmpeg = FFmpeg.concat(bassSetting);
     }
-    if(options && options['8d']){
-    let setting = ["-af","apulsator=hz=0.08"]
-    FFmpeg = FFmpeg.concat(setting)
+    if (options && options["8d"]) {
+      let setting = ["-af", "apulsator=hz=0.08"];
+      FFmpeg = FFmpeg.concat(setting);
+    }
+    if (options && options.pulsator) {
+      let setting = ["-af", "apulsator=hz=1"];
+      FFmpeg = FFmpeg.concat(setting);
+    }
+    if (options && options.karaoke) {
+      let setting = ["-af", "stereotools=mlev=0.03"];
+      FFmpeg = FFmpeg.concat(setting);
     }
     const transcoder = new prism.FFmpeg({
       args: FFmpeg,
@@ -52,7 +61,14 @@ export default class {
       channels: 2,
       frameSize: 960,
     });
-    let out = transcoder.pipe(opus);
+    let out = transcoder
+      .pipe(
+        new prism.VolumeTransformer({
+          type: "s16le",
+          volume: options.volume ? options.volume : 1,
+        })
+      )
+      .pipe(opus);
     out.on("close", () => {
       transcoder.destroy();
       opus.destroy();
